705fb8989b4fd8d56d82eef373e1774519505da0
Jose Antonio Chavez Verdin <caronte_1990@hotmail.com>
2014-06-06 17:00:54 -0600

    pagina
    pa

---
+++ untitled.py
@@ -0,0 +1,103 @@
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/07/cb_resp20140214.sql.bz2" "07"            
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/07/cb_resp20140216.sql.bz2" "07"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/07/coppel_resp20140214.sql.bz2" "07"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/07/coppel_resp20140216.sql.bz2" "07"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/cb_resp20140217.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/cb_resp20140218.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/cb_resp20140219.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/cb_resp20140220.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/cb_resp20140221.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/cb_resp20140222.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/cb_resp20140226.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/cb_resp20140227.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140217.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140218.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140219.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140220.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140222.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140223.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140225.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140226.sql.bz2" "08"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/2014/08/coppel_resp20140227.sql.bz2" "08"
+
+
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/files/bcoacl20140302.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcoacl20140309.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcobuxpuntual20140302.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcobuxpuntual20140309.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcoc120140302.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcoc120140309.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcocas20140302.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcocas20140309.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcocb20140302.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcocb20140309.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcocrece20140302.tar.bz2" "06"
+/home/cbbackup/bin/encriptar.sh "/mnt/backup/database/bcocrece20140309.tar.bz2" "06"   
+
+
+
+
+
+
+//semana0 07
+
+cb_resp20140210.sql.bz2.gpg
+cb_resp20140211.sql.bz2.gpg
+cb_resp20140212.sql.bz2.gpg
+cb_resp20140213.sql.bz2.gpg
+cb_resp20140214.sql.bz2.gpg
+cb_resp20140215.sql.bz2.gpg
+cb_resp20140216.sql.bz2.gpg
+coppel_resp20140210.sql.bz2.gpg
+coppel_resp20140211.sql.bz2.gpg
+coppel_resp20140212.sql.bz2.gpg
+coppel_resp20140213.sql.bz2.gpg
+coppel_resp20140214.sql.bz2.gpg
+coppel_resp20140215.sql.bz2.gpg
+coppel_resp20140216.sql.bz2.gpg
+
+semana 08
+cb_resp20140217.sql.bz2.gpg
+cb_resp20140218.sql.bz2.gpg
+cb_resp20140219.sql.bz2.gpg
+cb_resp20140220.sql.bz2.gpg
+cb_resp20140221.sql.bz2.gpg
+cb_resp20140222.sql.bz2.gpg
+coppel_resp20140217.sql.bz2.gpg
+coppel_resp20140218.sql.bz2.gpg
+coppel_resp20140219.sql.bz2.gpg
+coppel_resp20140220.sql.bz2.gpg
+coppel_resp20140221.sql.bz2.gpg
+coppel_resp20140222.sql.bz2.gpg
+coppel_resp20140223.sql.bz2.gpg
+
+//semana 09
+cb_resp20140224.sql.bz2.gpg
+cb_resp20140226.sql.bz2.gpg
+cb_resp20140227.sql.bz2.gpg
+coppel_resp20140224.sql.bz2.gpg
+coppel_resp20140225.sql.bz2.gpg
+coppel_resp20140226.sql.bz2.gpg
+coppel_resp20140227.sql.bz2.gpg
+
+
+
+ 
+ 
+
+    
+
+
+
+ 
+
+
+
+ 
+
+
+ 
+
+
+
+
